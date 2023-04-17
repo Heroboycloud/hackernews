c322b9ea07c97458cc32b7e173eb502b9f0814d8
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-04-17 12:36:07 +0100

    commited new app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Avatar,Username,Tweet} from './components.js';
+
+
+function App(){
+return(
+<div className="tweet">
+<Avatar />
+<Username />
+<Tweet />
+
+</div>
+);
+
+}
+
+
+
+export default App;
---
+++ src/components.js
@@ -0,0 +1,14 @@
+//import {useEffect,useState} from 'react';
+
+
+function Avatar(){
+return "Av"
+}
+function Username(){
+return "user"
+}
+function Tweet(){
+return "tweet"
+}
+
+export {Avatar,Username,Tweet}
